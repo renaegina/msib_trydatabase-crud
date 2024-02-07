721416d418ad24583d94e9a8dbf59a9782580525
renaegina <renaegina25778@gmail.com>
2024-02-07 18:08:20 +0700

    Update database connection

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 define("DB_HOST", "localhost:3307"); 
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
-define("DB_DATABASE", "try_database_crud");
+define("DB_DATABASE", "try-database-crud");
 
 // Membuat koneksi menuju database
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
